f67666f59cdd5e96d775842d3e70bf3d3f96a554
dayan_yamin <yamindayan@gmail.com>
2015-03-26 12:28:02 -0400

    Find Working for all

--- resources/php/find.php
+++ resources/php/find.php
@@ -1,28 +1,12 @@
 <?php
 
-$uname = "root";
-$pw = "123";
-$dbname = "adoptaprof";
-$dbAddress = "localhost";
-$conn = new mysqli($dbAddress, $uname, $pw, $dbname);
+require_once('RequestResponse.php');
+require_once('AuthenticationManager.php');
 
-if ($conn->connect_error)
-    die("Connection failed: " . $conn->connect_error);
+$params = $_GET?$_GET:$_POST;
+$username = $params['username'];
 
-if ($result = $conn->query("
-    SELECT username, firstname, lastname, users.user_id, title, website, email, path FROM users
-    LEFT JOIN pictures
-    ON users.user_id=pictures.user_id
-")
-) {
-    $i = 0;
-    while ($row = $result->fetch_array(MYSQL_ASSOC)) {
-        $res[$i] = $row;
-        $i++;
-    }
-
-    echo json_encode($res);
-
-    /* free result set */
-    $result->close();
-}
+$am = new AuthenticationManager();
+$res = $am->getProfessors();
+if($res) die(json_encode (new GetProfileInfoResponse(true,$res['errMsg'],$res)));
+die(json_encode (new RequestResponse(false,null)));
