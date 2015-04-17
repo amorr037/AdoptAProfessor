88f116b9d73d565315e962b8acc95a551d80eb6e
Javier Carmona <javier@mannywilson.com>
2015-04-16 21:55:52 -0400

    finished delete user

---
+++ resources/php/DeleteUser.php
@@ -0,0 +1,9 @@
+<?php
+require_once('RequestResponse.php');
+require_once('AuthenticationManager.php');
+$params = $_GET?$_GET:$_POST;
+$username = $params['username'];
+$am = new AuthenticationManager();
+$res = $am->deleteUser($username);
+if($res) die(json_encode (new RequestResponse(false,$res['errMsg'])));
+die(json_encode (new RequestResponse(true,null)));
