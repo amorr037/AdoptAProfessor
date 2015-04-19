d3f7414abc104d8b818aa7fbdbe50cd870769e60
dayan_yamin <yamindayan@gmail.com>
2015-04-19 19:37:23 -0400

    invite fix

--- resources/php/inviteNewProfessor.php
+++ resources/php/inviteNewProfessor.php
@@ -5,11 +5,11 @@ require_once('RequestResponse.php');
 
 $params = $_GET?$_GET:$_POST;
 $email = $params['email'];
-$subject = $params['subject'];
+$message = $params['message'];
 $authmanager = new AuthenticationManager();
 if($errMsg['errMsg'] != null) die(json_encode (new RequestResponse(false,$errMsg)));
 //die(json_encode (new RequestResponse(true,$errMsg)));
-$emailSentResponse = $authmanager->sendNewInvitation($email, $subject);
+$emailSentResponse = $authmanager->sendNewInvitation($email, $message);
 if($emailSentResponse['errMsg'] != null)
 {
     $emailSentResponse['errMsg'] = "Unable to send email at this time!";
